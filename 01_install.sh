cfb319667bb44675b7bc3c22bb1a1b4abaef103d
Chris <christianbuchert@gmail.com>
2019-09-12 16:58:59 -0600

    Added an install script to install Homebrew dependencies.

---
+++ 01_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install XCode
+xcode-select --install
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install the basics
+for i in $(cat applications/homebrew_formula.txt)
+do
+	brew install "$i"
+done
+
+brew tap caskroom/cask
+for i in $(cat applications/homebrew-cask_formula.txt)
+do
+	brew install "$i"
+done
+
+brew cleanup
+brew cask cleanup
+
+# Set up Node
+nvm install latest
+
+# Configure ZSH
+$ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s $(which zsh)
